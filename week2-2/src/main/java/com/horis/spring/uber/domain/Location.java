ec0599cd33dc4ddb49e418b85010de9c1700c00a
horis233 <horis233@gmail.com>
2018-12-21 23:40:47 -0500

    :construction: LocationRepository Interface extends from JPA Repository

--- week2-2/src/main/java/com/horis/spring/uber/domain/Location.java
+++ week2-2/src/main/java/com/horis/spring/uber/domain/Location.java
@@ -1,10 +1,18 @@
 package com.horis.spring.uber.domain;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import javax.persistence.*;
 import java.util.Date;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@RequiredArgsConstructor
+@Entity
+@Table(name = "LOCATION")
 
 public class Location {
 
